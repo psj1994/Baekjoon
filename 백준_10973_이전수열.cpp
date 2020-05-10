fb890a37c12ecaf8aa3dc954c723551253282243
psj1994 <65104789+psj1994@users.noreply.github.com>
2020-05-10 16:44:21 +0900

    Add files via upload

---
+++ 백준_10973_이전수열.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(void)
+{
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	for(int i=0;i<a.size();i++)
+	{
+		cin>>a[i];
+	}
+	if(prev_permutation(a.begin(),a.end()))
+	{
+		for(auto & w : a)cout<<w<<' ';
+	}else{
+		cout<<-1<<' ';
+	}
+	return 0;
+}
